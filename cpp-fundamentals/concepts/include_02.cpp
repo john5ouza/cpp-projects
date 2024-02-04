8a7b1988b13c917a868e6c01d9f718a829dc9aef
john5ouza <john5souz4@gmail.com>
2024-02-04 15:28:01 -0300

    Some concepts about C++

---
+++ cpp-fundamentals/concepts/include_02.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include "includes/include_02.h"
+
+using namespace std;
+
+int main()
+{
+  cout << add(5, 8) << endl;
+  
+  return 0;
+}
+
+int add (int x, int y) {
+  return x + y;
+}
